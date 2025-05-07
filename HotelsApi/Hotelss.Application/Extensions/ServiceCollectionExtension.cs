1ae3bf5c6cc780f70d31d2521ae1e168dee2f635
Steilor <steilormateo.44@gmail.com>
2025-05-07 19:42:34 -0400

    Adding MediatR

--- HotelsApi/Hotelss.Application/Extensions/ServiceCollectionExtension.cs
+++ HotelsApi/Hotelss.Application/Extensions/ServiceCollectionExtension.cs
@@ -1,22 +1,21 @@
-﻿using Hotelss.Application.Hotels;
-using Hotelss.Domain.Repositories;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Hotelss.Application.Hotels;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hotelss.Application.Extensions
 {
-    public static class ServiceCollectionExtension
+    public static class ServiceCollectionExtensions
     {
         public static void AddApplication(this IServiceCollection services)
         {
+          var applicationAssembly  = typeof(ServiceCollectionExtensions).Assembly;
           services.AddScoped<IHotelsService, HotelsService>();
 
-            services.AddAutoMapper(typeof(ServiceCollectionExtension).Assembly);
+            services.AddAutoMapper(applicationAssembly);
 
+            services.AddValidatorsFromAssembly(applicationAssembly)
+                .AddFluentValidationAutoValidation();
         
         }
     }
--- HotelsApi/Hotelss.Application/Validators/CreateHotelDtoValidator.cs
+++ HotelsApi/Hotelss.Application/Validators/CreateHotelDtoValidator.cs
@@ -5,9 +5,33 @@ namespace Hotelss.Application.Validators;
 
 public class CreateHotelDtoValidator: AbstractValidator<CreateHotelDto>
 {
+    private readonly List<string> validCategories = ["Luxury", "Boutique", "Budget", "Resort", "Business", "All-Inclusive", 
+        "Hostel", "Bed & Breakfast", "Aparthotel"];
     public CreateHotelDtoValidator()
     {
         RuleFor(dto => dto.Nombre)
-            .Length(3, 100);
+              .Length(3, 100);
+
+        RuleFor(dto => dto.Category)
+            .Must(validCategories.Contains)
+            .WithMessage("Invalid category. Please choose from the valid categories.");
+         //.Custom((value, context) =>
+         //{
+         //    var isValidCategory = validCategories.Contains(value);
+
+         //    if (!isValidCategory)
+         //    {
+         //        context.AddFailure("Category", "Invalid category. Please choose from the valid categories.");
+         //    }
+         //});
+
+        RuleFor(dto => dto.ContactEmail)
+            .EmailAddress()
+            .WithMessage("Please provide a valid email.");
+
+        RuleFor(dto => dto.PostalCode)
+            .Matches(@"^\d{2}-\d{3}$")
+            .WithMessage("Please provide a valid postal code (XX-XXX).");
+
     }
 }
