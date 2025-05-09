2fd06e4fcbb5de80a4c15c17b36d7ade26ef2720
Steilor <steilormateo.44@gmail.com>
2025-05-09 18:39:33 -0400

    Adding MediatR

---
+++ HotelsApi/Hotelss.Application/Hotels/Commands/CreateHotel/CreateHotelCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Hotelss.Application.Hotels.Commands.CreateHotel;
+
+public class CreateHotelCommand : IRequest<int>
+{
+    public string Nombre { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string Category { get; set; } = default!;
+    public bool IsAvailable { get; set; }
+    public string? ContactEmail { get; set; }
+    public string? ContactNumber { get; set; }
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+}
---
+++ HotelsApi/Hotelss.Application/Hotels/Commands/CreateHotel/CreateHotelCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Hotelss.Application.Hotels.Commands.CreateHotel
+{
+    public class CreateHotelCommandHandler : IRequestHandler<CreateHotelCommand, int>
+    {
+        public Task<int> Handle(CreateHotelCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+ 
