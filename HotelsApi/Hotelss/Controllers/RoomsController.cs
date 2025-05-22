a28024c52c2088bbbfc07975239382b0aa12969b
Steilor <steilormateo.44@gmail.com>
2025-05-22 17:19:26 -0400

    adding Room Entity

--- HotelsApi/Hotelss/Controllers/RoomsController.cs
+++ HotelsApi/Hotelss/Controllers/RoomsController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hotelss.API.Controllers;
 
 [Route("api/hotel/{hotelId}/rooms")]
 [ApiController]
-public class RoomsController : Controller
+public class RoomsController(IMediator mediator) : Controller
 {
     [HttpPost]
     public async Task<IActionResult> CreateRoom([FromRoute] int hotelId, CreateRoomCommand command)
     {
-
+        await mediator.Send(command);
+        return Created();
     }
 }
