59bafdea847a23156764f887deb52ed777c78bed
Steilor <steilormateo.44@gmail.com>
2025-06-13 23:54:28 -0400

    Pagination to the GetAll

---
+++ HotelsApi/Hotelss.Application/Hotels/Queries/GetAllHotels/GetAllHotelsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelss.Application.Hotels.Queries.GetAllHotels
+{
+    internal class GetAllHotelsQueryValidator
+    {
+    }
+}
