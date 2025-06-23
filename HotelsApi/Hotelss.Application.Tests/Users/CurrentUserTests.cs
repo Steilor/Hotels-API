e84f07e29480c3fc2fb7410d555f2d84ca819f77
Steilor <steilormateo.44@gmail.com>
2025-06-23 18:59:36 -0400

    Adding UnitTest

---
+++ HotelsApi/Hotelss.Application.Tests/Users/CurrentUserTests.cs
@@ -0,0 +1,20 @@
+﻿using Hotelss.Domain.Constants;
+using Xunit;
+
+namespace Hotelss.Application.Users.Tests;
+
+public class CurrentUserTests
+{
+    [Fact()]
+    public void IsInRole_WithMachingRole_ShouldReturnTrue()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@gmail.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+
+        currentUser.IsInRole(UserRoles.Admin);
+        // assert
+
+    }
+}
