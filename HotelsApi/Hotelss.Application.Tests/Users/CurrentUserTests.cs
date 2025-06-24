e68bee821f44f66c890c55bcb27dcdd8021ad3f2
Steilor <steilormateo.44@gmail.com>
2025-06-24 18:59:47 -0400

    CurrentUserTests

--- HotelsApi/Hotelss.Application.Tests/Users/CurrentUserTests.cs
+++ HotelsApi/Hotelss.Application.Tests/Users/CurrentUserTests.cs
@@ -1,21 +1,52 @@
-﻿using Hotelss.Domain.Constants;
+﻿using FluentAssertions;
+using Hotelss.Domain.Constants;
 using Xunit;
 
 namespace Hotelss.Application.Users.Tests;
 
 public class CurrentUserTests
 {
-    [Fact()]
-    public void IsInRole_WithMachingRole_ShouldReturnTrue()
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData(UserRoles.User)]
+    public void IsInRole_WithMachingRole_ShouldReturnTrue(string roleName)
     {
         // arrange
         var currentUser = new CurrentUser("1", "test@gmail.com", [UserRoles.Admin, UserRoles.User], null, null);
 
         // act
 
-        var isInRole = currentUser.IsInRole(UserRoles.Admin);
+        var isInRole = currentUser.IsInRole(roleName);
         // assert
 
-        isInRole.Should()
+        isInRole.Should().BeTrue();
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMachingRole_ShouldReturnFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@gmail.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+
+        var isInRole = currentUser.IsInRole(UserRoles.Owner);
+        // assert
+
+        isInRole.Should().BeFalse();
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMachingRoleCase_ShouldReturnFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@gmail.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+
+        var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+        // assert
+
+        isInRole.Should().BeFalse();
     }
 }
