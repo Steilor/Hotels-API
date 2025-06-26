2f86afd2fc0d34f81c5634b6384382fc76c67f45
Steilor <steilormateo.44@gmail.com>
2025-06-26 15:10:10 -0400

    Arrange UserContextTests

--- HotelsApi/Hotelss.Application.Tests/Users/UserContextTests.cs
+++ HotelsApi/Hotelss.Application.Tests/Users/UserContextTests.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using Hotelss.Domain.Constants;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using Xunit;
 
 namespace Hotelss.Application.Users.Tests;
 
@@ -8,7 +12,27 @@ public class UserContextTests
     public void GetCurrentUser_WithAuthenticadedUser_ShouldRerturnCurrentUser()
     {
         // arrange
+        var dateOfBirth = new DateOnly(1990, 1, 1);
 
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.NameIdentifier, "1"),
+            new(ClaimTypes.Email, "test@test.com"),
+            new(ClaimTypes.Role, UserRoles.Admin),
+            new(ClaimTypes.Role, UserRoles.User),
+            new("Nationality", "German"),
+            new("DateOfBirth", dateOfBirth.ToString("yyyy-MM-dd"))
+        };
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+        httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+        {
+            User = user
+        });
+
+        var userContext = new UserContext(httpContextAccessorMock.Object);
         // act
 
         // asset
