9277ae144288b6d1c87b66f41d244d857f259dcc
Steilor <steilormateo.44@gmail.com>
2025-06-29 18:05:05 -0400

    CreateHotelCommand test validator

--- HotelsApi/Hotelss.Application.Tests/Hotels/Commands/CreateHotel/CreateHotelCommandValidatorTests.cs
+++ HotelsApi/Hotelss.Application.Tests/Hotels/Commands/CreateHotel/CreateHotelCommandValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using FluentValidation.TestHelper;
+using Xunit;
 
 namespace Hotelss.Application.Hotels.Commands.CreateHotel.Tests;
 
@@ -16,5 +17,42 @@ public class CreateHotelCommandValidatorTests
             ContactEmail = "test@test.com",
             PostalCode = "12-345",
         };
+
+        var validator = new CreateHotelCommandValidator();
+
+        // act
+
+        var result = validator.TestValidate(command);
+
+        // assert
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact()]
+    public void Validator_ForInvalidCommand_ShouldHaveValidationErrors()
+    {
+        // arrange
+
+        var command = new CreateHotelCommand()
+        {
+            Nombre = "Te",
+            Category = "None",
+            ContactEmail = "test@",
+            PostalCode = "12345",
+        };
+
+        var validator = new CreateHotelCommandValidator();
+
+        // act
+
+        var result = validator.TestValidate(command);
+
+        // assert
+
+        result.ShouldHaveValidationErrorFor(c=> c.Nombre);
+        result.ShouldHaveValidationErrorFor(c => c.Category);
+        result.ShouldHaveValidationErrorFor(c => c.ContactEmail);
+        result.ShouldHaveValidationErrorFor(c => c.PostalCode);
     }
 }
