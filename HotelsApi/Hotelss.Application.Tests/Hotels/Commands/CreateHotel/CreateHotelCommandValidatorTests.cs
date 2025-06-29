cc2750d7b708ba6e2375b367ce7f0f45bc430d6c
Steilor <steilormateo.44@gmail.com>
2025-06-29 17:49:34 -0400

    CreateHotelCommand test validator

---
+++ HotelsApi/Hotelss.Application.Tests/Hotels/Commands/CreateHotel/CreateHotelCommandValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Hotelss.Application.Hotels.Commands.CreateHotel.Tests;
+
+public class CreateHotelCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // arrange
+
+        var command = new CreateHotelCommand()
+        {
+            Nombre = "Test",
+            Category = "Boutique",
+            ContactEmail = "test@test.com",
+            PostalCode = "12-345",
+        };
+    }
+}
