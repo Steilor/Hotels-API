803c5fde6f0595a1a87cdb53cbaed78d90e819a8
Steilor <steilormateo.44@gmail.com>
2025-06-29 20:01:55 -0400

    Mapping test

---
+++ HotelsApi/Hotelss.Application.Tests/Hotels/Dtos/HotelsProfileTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Hotelss.Domain.Entities;
+using Xunit;
+
+namespace Hotelss.Application.Hotels.Dtos.Tests;
+
+public class HotelsProfileTests
+{
+    [Fact()]
+    public void CreateMap_ForHotelToHotelsDto_MapsCorrectly()
+    { 
+       // arrange
+
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<HotelsProfile>();
+        });
+
+        var mapper = configuration.CreateMapper();
+
+        var hotel = new Hotel()
+        {
+            Id = 1,
+            Nombre = "Test hotel",
+            Description = "Test Description",
+            Category = "Test Category",
+            IsAvailable = true,
+            ContactEmail = "test@example.com",
+            ContactNumber = "123456789",
+            Address = new Address
+            {
+                City = "Test City",
+                Street = "Test Street",
+                PostalCode = "12345"
+            }
+        };
+    }
+}
